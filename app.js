e38a083ea867b0ec67d7ef81ed09ed9e57e9bd34
Hansen-2024 <163928840+Hansen-2024@users.noreply.github.com>
2026-06-28 19:09:50 -0500

    Update app.js

--- app.js
+++ app.js
@@ -430,7 +430,7 @@ function buildKCGrid() {
     grid.style.display = "grid";
     grid.style.width = "100%";
     grid.style.minWidth = "unset";
-    grid.style.gridTemplateColumns = `80px 90px repeat(${Kvalues.length},1fr)`;
+    grid.style.gridTemplateColumns = `80px 90px repeat(${Kvalues.length},minmax(120px, 1fr))`;
     grid.style.gap = "2px";
     grid.style.alignItems = "center";
 
